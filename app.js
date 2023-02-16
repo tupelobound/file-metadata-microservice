c681ce4fe47fcafd1790dee9103fdc62466f9ac1
tupelobound <29886944+tupelobound@users.noreply.github.com>
2023-02-16 19:12:34 +0000

    Added file upload functionality

--- app.js
+++ app.js
@@ -2,9 +2,17 @@
 const express = require('express')
 const app = express()
 const port = 3000
+// use express-fileupload package
+const fileUpload = require('express-fileupload');
+app.use(fileUpload());
 
 // serve static assets from the 'public' directory
 app.use(express.static('public'))
+
+// get file upload from POST and send response as JSON
+app.post('/api/fileanalyse', function(req, res){
+  res.json({"name":req.files.upfile.name, "type":req.files.upfile.mimetype, "size":req.files.upfile.size });
+});
 
 // listen for requests
 app.listen(port, () => {
